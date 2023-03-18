38dedb14179383949451555e559c82a71543cb6a
Samie Azubike <85092681+samieteq@users.noreply.github.com>
2023-03-18 15:22:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
 				<div className="flex relative top-[80px] justify-around">
 					<div className="w-[50%]">
 						<label className="font-bold text-[80px] leading-none pt-[40px] tracking-tight">
-							I'm a{" "}
+							I&apos;m a
 							<span className="text-violet-800">
 								Software
 								<br /> Developer
