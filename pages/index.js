1a51fe97578c7b79cc338ce899da145a48d28483
samieteq <codewithsamie@gmail.com>
2023-03-20 00:47:30 +0100

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Image from "next/image";
 export default function Home() {
 	return (
 		<>
@@ -15,37 +14,49 @@ export default function Home() {
 					content="width=device-width, initial-scale=1"
 				/>
 				<link rel="icon" href="/favicon.ico" />
+				<link
+					href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+					rel="stylesheet"></link>
+				{/* <script
+					src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"
+					async></script> */}
 			</Head>
-			<main className="container">
-				<div className="flex items-center">
+			<main className="px-[120px] relative">
+				<div className="flex items-center justify-between">
 					<a
 						href="#"
-						className="py-7 text-4xl font-bold no-underline relative col-5">
+						className="py-7 text-4xl font-bold no-underline relative">
 						Samie Azubike
 					</a>
-					<div className="row">
-						<a href="#" className="no-underline col">
+					<div className="">
+						<a href="#" className="no-underline px-4 font-bold">
 							About
 						</a>
-						<a href="#" className="no-underline col">
+						<a href="#" className="no-underline px-4 font-bold">
 							Skills
 						</a>
-						<a href="#" className="no-underline col">
+						<a href="#" className="no-underline px-4 font-bold">
 							Portfolio
 						</a>
 					</div>
-					<div className="col-5">
-						<a href="#" className="col-5">
+					<div className="">
+						<a
+							href="https://drive.google.com/file/d/1m-Q5CZCYU5Y0E4XAeajOC7UXhTUFpSHy/view?usp=sharing"
+							className="border-2 rounded-[6px] border-violet-800 px-6 py-3 font-bold text-violet-800">
 							Resume
 						</a>
 					</div>
 				</div>
-				<div className="flex relative top-[80px] justify-around">
+				<div className="flex relative top-[80px] justify-between">
 					<div className="w-[50%]">
 						<label className="font-bold text-[80px] leading-none pt-[40px] tracking-tight">
-							I&apos;m a <span className="text-violet-800">Software<br />Developer</span>
+							I&apos;m a{" "}
+							<span className="text-violet-800">
+								Software
+								<br /> Developer
+							</span>
 						</label>
-						<p className="w-[80%] text-[18px] font-medium relative top-[20px]">
+						<p className="w-[90%] text-[17px] font-medium relative top-[20px]">
 							I have a year of experience building and designing
 							software, Currently, I love to work on web
 							application using technologies like, Java,
@@ -53,17 +64,40 @@ export default function Home() {
 						</p>
 					</div>
 					<div className="w-[350px] h-[350px] bg-[#0DB760] relative rounded before:content-[''] before:absolute  before:w-[335px] before:h-[325px] before:border-4 before:border-[#000] before:-left-[30px] before:top-[50px] before:rounded-md bg-gradient-to-r from-violet-500 to-fuchsia-500">
-						<div className="hero-avatar"></div>
-						<img
+						<Image
+							alt="samie azubike | software developer"
 							src="/MAT_6779.png"
 							className="z-20 absolute bottom-[0]"
+							width={350}
+							height={300}
 						/>
 					</div>
-					<div className="absolute right-0">
-						<label>Follow me on:</label>
-					</div>
 				</div>
+				<SocialLink />
 			</main>
 		</>
+	);
+}
+
+function SocialLink() {
+	return (
+		<div className="absolute rotate-90 right-[-105px] top-[350px]  text-violet-800 text-semibold flex items-center">
+			<div className="flex items-center">
+				<label className="px-5">Follow me on:</label>
+				<hr className="w-12 border-0 h-px bg-violet-800" />
+			</div>
+
+			<div className="px-5 flex gap-4">
+				<a href="#" className="">
+					<i class="bx bxl-twitter bx-rotate-270 text-3xl"></i>
+				</a>
+				<a href="#">
+					<i class="bx bxl-linkedin-square bx-rotate-270 text-3xl"></i>
+				</a>
+				<a href="#">
+					<i class="bx bxl-github bx-rotate-270 text-3xl"></i>
+				</a>
+			</div>
+		</div>
 	);
 }
