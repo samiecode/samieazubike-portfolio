6d149682ac9e10d4175c5d6e6dcb47568ecc3e54
samieteq <codewithsamie@gmail.com>
2023-03-21 08:46:57 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -124,7 +124,7 @@ export default function Home() {
 
 function Contact() {
 	return (
-		<div className="py-[70px] xs:max-sm:w-[100%] md:max-lg:flex flex-col">
+		<div className="py-[70px] xs:max-sm:w-[100%] xs:max-lg:flex md:max-lg:flex flex-col">
 			<a
 				href="#"
 				className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white py-4 px-8 rounded font-bold text-center text-md xs:max-sm:w-[100%] xs:max-sm:px-2 ">
@@ -150,13 +150,13 @@ function SocialLink() {
 
 			<div className="flex flex-col gap-4 relative top-[100px] xs:max-md:-top-[20px] xs:max-lg:-left-[30px] sm:max-lg:top-[50px] md:max-lg:top-[80px]  md:max-lg:-left-[50px] ">
 				<a href="https://www.twitter.com/samieteq" className="">
-					<i class="bx bxl-twitter bx-rotate-270 text-3xl transform transition-all hover:scale-150 sm:max-lg:text-5xl"></i>
+					<i class="bx bxl-twitter bx-rotate-270 text-3xl transform transition-all hover:scale-150 sm:max-lg:text-5xl xs:max-md:text-2xl"></i>
 				</a>
 				<a href="https://www.linkedin.com/in/samieteq">
-					<i class="bx bxl-linkedin-square bx-rotate-270 text-3xl transform transition-all hover:scale-150 hover:rotate-10 sm:max-lg:text-5xl"></i>
+					<i class="bx bxl-linkedin-square bx-rotate-270 text-3xl transform transition-all hover:scale-150 hover:rotate-10 sm:max-lg:text-5xl xs:max-md:text-2xl "></i>
 				</a>
 				<a href="https://www.github.com/samieteq">
-					<i class="bx bxl-github text-3xl transform transition-all rotate-270 hover:scale-150 sm:max-lg:text-5xl"></i>
+					<i class="bx bxl-github text-3xl transform transition-all rotate-270 hover:scale-150 sm:max-lg:text-5xl xs:max-md:text-2xl"></i>
 				</a>
 			</div>
 		</div>
