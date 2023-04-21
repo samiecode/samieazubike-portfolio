a1f257985e0c97665a72968a43f06dcc1ff25cca
samieteq <codewithsamie@gmail.com>
2023-04-21 10:05:34 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -366,7 +366,7 @@ const PortFolio = () => {
 				<h1 className="z-20 text-black relative before:-bottom-1 font-bold text-6xl before:z-10 before:content-[''] before:bg-gradient-to-r before:from-violet-500 before:to-fuchsia-500 before:absolute before:w-[245px] before:h-[7px] before:bottom-0 before:left-0 ">
 					Portfolio
 				</h1>
-				<p className="text-[22px] mt-10"></p>
+				<p className="text-[22px] mt-10">To be added soon...</p>
 			</div>
 		</>
 	);
