a8034c7f58d0accf1ea7ad2854967aaffbf169ae
samieteq <codewithsamie@gmail.com>
2023-05-03 12:47:58 +0100

    portfolio component updated

--- pages/component/PortFolio.jsx
+++ pages/component/PortFolio.jsx
@@ -7,7 +7,15 @@ const PortFolio = () => {
 				<h1 className="z-20 text-black relative before:-bottom-1 font-bold text-6xl before:z-10 before:content-[''] before:bg-gradient-to-r before:from-violet-500 before:to-fuchsia-500 before:absolute before:w-[245px] before:h-[7px] before:bottom-0 before:left-0 ">
 					Portfolio
 				</h1>
-				<div className="flex w-full">
+				<div className="flex flex-row justify-between">
+					<Project
+						link="https://github.com/samieteq/bobfi"
+						image="bobfi-splash-screen"
+					/>
+					<Project
+						link="https://github.com/samieteq/bobfi"
+						image="bobfi-splash-screen"
+					/>
 					<Project
 						link="https://github.com/samieteq/bobfi"
 						image="bobfi-splash-screen"
