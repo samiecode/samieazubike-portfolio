74031b3222e53c621b396fae6bf9e29ab7f14502
Samie Azubike <codewithsamie@gmail.com>
2023-09-10 17:07:05 +0100

    Updated

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -113,7 +113,7 @@ const Skill = () => {
 			{skills.map((item, key) => (
 				<div
 					key={key}
-					className={`h-[130px] w-[150px] bg-white drop-shadow-md rounded-md flex justify-center items-center transition transform hover:scale-110 flex-col hover:border ${item.hover_border} xs:max-md:h-[80px] xs:max-md:w-[80px]`}
+					className={`h-[120px] w-[140px] bg-white drop-shadow-md rounded-md flex justify-center items-center transition transform hover:scale-110 flex-col hover:border ${item.hover_border} xs:max-md:h-[80px] xs:max-md:w-[80px]`}
 				>
 					<div
 						className={`py-3 px-3 ${item.border_color} border rounded-full flex justify-center items-center flex-col xs:max-md:py-0 xs:max-md:px-0 xs:max-md:w-[40px] xs:max-md:h-[40px]`}
